5f17124b0351c21cfdd3d424ba582597b2793e8e
aleksandr <atrostyanko@gmail.com>
2022-04-28 16:02:19 +0300

    TestNG ready

---
+++ src/test/java/BUnitTest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class BUnitTest extends BaseTest {
+
+    @Test
+    public void test() {
+        System.out.println("First Unit Test...");
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("Second Unit Test...");
+    }
+}
